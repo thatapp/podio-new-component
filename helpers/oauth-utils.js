986866c14ded1248ad62731202b011fb210df5d3
williamnwogbo <williamnwogbo@gmail.com>
2020-01-08 21:03:33 +0100

    ...

--- helpers/oauth-utils.js
+++ helpers/oauth-utils.js
@@ -16,47 +16,47 @@ var _ = require('lodash');
  * @param next
  */
 async function refreshToken(serviceUri, clientIdKey, clientSecretKey, conf, next) {
-    'use strict';
-
-    var clientId = getValueFromEnv(clientIdKey);
-    var clientSecret = getValueFromEnv(clientSecretKey);
-
-    // Now we need to resolve URI in case we have a replacement groups inside it
-    // for example for Salesforce we have a production and test environemnt
-    // or shopware the user domain is part of OAuth URIs
-    var refreshURI = resolveVars(serviceUri, conf);
-
-    var params = {
-        grant_type: "refresh_token",
-        client_id: clientId,
-        client_secret: clientSecret,
-        refresh_token: conf.oauth ? conf.oauth.refresh_token : null,
-        format: "json"
-    };
-
-    var newConf = _.cloneDeep(conf);
-
-   await httpUtils.getJSON({
-        url: refreshURI,
-        method: "post",
-        form: params
-    }, function processResponse(err, refreshResponse) {
-        if (err) {
-            console.error('Failed to refresh token from %s', serviceUri);
-            return next(err);
-        }
-        console.log('Refreshed token from %s', serviceUri);
-        // update access token in configuration
-        newConf.oauth.access_token = refreshResponse.access_token;
-        // if new refresh_token returned, update that also
-        // specification is here http://tools.ietf.org/html/rfc6749#page-47
-        if (refreshResponse.refresh_token) {
-            newConf.oauth.refresh_token = refreshResponse.refresh_token;
-        }
-        console.log(newConf);
-       next(newConf);
-      //  return newConf;
-    });
+    // 'use strict';
+return true;
+   //  var clientId = getValueFromEnv(clientIdKey);
+   //  var clientSecret = getValueFromEnv(clientSecretKey);
+   //
+   //  // Now we need to resolve URI in case we have a replacement groups inside it
+   //  // for example for Salesforce we have a production and test environemnt
+   //  // or shopware the user domain is part of OAuth URIs
+   //  var refreshURI = resolveVars(serviceUri, conf);
+   //
+   //  var params = {
+   //      grant_type: "refresh_token",
+   //      client_id: clientId,
+   //      client_secret: clientSecret,
+   //      refresh_token: conf.oauth ? conf.oauth.refresh_token : null,
+   //      format: "json"
+   //  };
+   //
+   //  var newConf = _.cloneDeep(conf);
+   //
+   // await httpUtils.getJSON({
+   //      url: refreshURI,
+   //      method: "post",
+   //      form: params
+   //  }, function processResponse(err, refreshResponse) {
+   //      if (err) {
+   //          console.error('Failed to refresh token from %s', serviceUri);
+   //          return next(err);
+   //      }
+   //      console.log('Refreshed token from %s', serviceUri);
+   //      // update access token in configuration
+   //      newConf.oauth.access_token = refreshResponse.access_token;
+   //      // if new refresh_token returned, update that also
+   //      // specification is here http://tools.ietf.org/html/rfc6749#page-47
+   //      if (refreshResponse.refresh_token) {
+   //          newConf.oauth.refresh_token = refreshResponse.refresh_token;
+   //      }
+   //      console.log(newConf);
+   //     next(newConf);
+   //    //  return newConf;
+   //  });
 }
 
 function getValueFromEnv(key) {
