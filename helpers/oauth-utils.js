c26104706c51fbb5b4050d703b5a909b03b91b98
williamnwogbo <williamnwogbo@gmail.com>
2020-09-02 19:33:09 +0100

    =Update dependencies

--- helpers/oauth-utils.js
+++ helpers/oauth-utils.js
@@ -23,49 +23,49 @@ async function refreshToken(serviceUri, clientIdKey, clientSecretKey,credentials
      var clientSecret = getValueFromEnv(clientSecretKey);
 
     // var refreshURI = resolveVars(serviceUri, conf);
-    var refreshURI = "https://thatapp-api.thatapp.io/api/update/token";
+    // var refreshURI = "https://thatapp-api.thatapp.io/api/update/token";
+    //
+    // var params = {
+    //     grant_type: "refresh_token",
+    //     client_id: clientId,
+    //     client_secret: clientSecret,
+    //     refresh_token: conf.oauth ? conf.oauth.refresh_token : null,
+    //     user_id: conf.oauth && typeof conf.oauth.ref != "undefined" ? conf.oauth.ref.id: null,
+    //     email: credentials.email ? credentials.email : null,
+    //     format: "json"
+    // };
 
-    var params = {
-        grant_type: "refresh_token",
-        client_id: clientId,
-        client_secret: clientSecret,
-        refresh_token: conf.oauth ? conf.oauth.refresh_token : null,
-        user_id: conf.oauth && typeof conf.oauth.ref != "undefined" ? conf.oauth.ref.id: null,
-        email: credentials.email ? credentials.email : null,
-        format: "json"
-    };
-
-    console.log("Calling Refresh Token");
-
-    var newConf = _.cloneDeep(conf);
-
-   await httpUtils.getJSON({
-        url: refreshURI,
-        method: "post",
-        form: params
-    }, function processResponse(err, refreshResponse) {
-        if (err) {
-            console.error('Failed to refresh token from %s', serviceUri);
-            return next(err);
-        }
-        console.log('Refreshed token from %s', serviceUri);
-        // update access token in configuration
-       if(refreshResponse.access_token) {
-           newConf.oauth.access_token = refreshResponse.access_token;
-           // if new refresh_token returned, update that also
-           // specification is here http://tools.ietf.org/html/rfc6749#page-47
-           if (refreshResponse.refresh_token) {
-               newConf.oauth.refresh_token = refreshResponse.refresh_token;
-           }
-       }else{
-           console.log('Cannot Generate token, kindly reverify, to apply new changes');
-           var er = "Cannot Generate token, kindly reverify, to apply new changes";
-           return next(er);
-       }
-        console.log(newConf);
-       next(newConf);
-      //  return newConf;
-    });
+   //  console.log("Calling Refresh Token");
+   //
+   //  var newConf = _.cloneDeep(conf);
+   //
+   // await httpUtils.getJSON({
+   //      url: refreshURI,
+   //      method: "post",
+   //      form: params
+   //  }, function processResponse(err, refreshResponse) {
+   //      if (err) {
+   //          console.error('Failed to refresh token from %s', serviceUri);
+   //          return next(err);
+   //      }
+   //      console.log('Refreshed token from %s', serviceUri);
+   //      // update access token in configuration
+   //     if(refreshResponse.access_token) {
+   //         newConf.oauth.access_token = refreshResponse.access_token;
+   //         // if new refresh_token returned, update that also
+   //         // specification is here http://tools.ietf.org/html/rfc6749#page-47
+   //         if (refreshResponse.refresh_token) {
+   //             newConf.oauth.refresh_token = refreshResponse.refresh_token;
+   //         }
+   //     }else{
+   //         console.log('Cannot Generate token, kindly reverify, to apply new changes');
+   //         var er = "Cannot Generate token, kindly reverify, to apply new changes";
+   //         return next(er);
+   //     }
+   //      console.log(newConf);
+   //     next(newConf);
+   //    //  return newConf;
+   //  });
 
 
 
