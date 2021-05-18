df76867024ecf22e93aba68c66dcaa370b0170db
jboy457 <adejareemma@gmail.com>
2021-05-18 17:19:28 +0100

    fix: delete comment and split field

--- helpers/itemHelper.js
+++ helpers/itemHelper.js
@@ -40,9 +40,9 @@ exports.emitData = async (cfg,result,that,end = null) => {
             const output = messages.newMessageWithBody(i_item);
             await that.emit('data', output);
         }
-        if(end === null) {
-            await that.emit('end');
-        }
+        // if(end === null) {
+        //     await that.emit('end');
+        // }
     } else {
         that.emit('data', messages.newMessageWithBody(result));
     }
